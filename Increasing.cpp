a284bc5c4eb67e3df513e4f56024b7bdf3b9f653
mahfuj10 <mahfujglobal@gmail.com>
2023-06-27 14:05:29 +0600

    B. increasing

---
+++ Increasing.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> arr[i];
+        }
+
+        sort(arr, arr + n);
+
+        bool isStrictly = 1;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            if (arr[i] == arr[i + 1])
+            {
+                isStrictly = 0;
+            }
+        }
+
+        if (isStrictly == 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
