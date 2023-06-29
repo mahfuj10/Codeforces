f65b9659e5117eedfcb8a43f213928fb7428a4a3
mahfuj10 <mahfujglobal@gmail.com>
2023-06-29 09:35:00 +0600

    1030A - In Search of an Easy Problem

---
+++ 1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n == 1)
+        {
+            cout << "HARD" << endl;
+            return 0;
+        }
+    }
+
+    cout << "EASY" << endl;
+
+    return 0;
+}
