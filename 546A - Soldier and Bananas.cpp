280231abbe1348234a8f457741cf887e8af8f11c
mahfuj10 <mahfujglobal@gmail.com>
2023-06-29 09:09:02 +0600

    Sodier and Bananas

---
+++ 546A - Soldier and Bananas.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, n, w;
+    int sum = 0;
+
+    cin >> k >> n >> w;
+
+    for (int i = 1; i <= w; ++i)
+    {
+        sum += k * i;
+    }
+
+    if (sum <= n)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << sum - n << endl;
+    }
+
+    return 0;
+}
