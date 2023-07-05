ee15a7b2f20101d9874cd0dc1ed3c707c2564450
mahfuj10 <mahfujglobal@gmail.com>
2023-07-05 18:10:39 +0600

    A. Beautiful year

---
+++ 271A - Beautiful Year.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+
+    while (true)
+    {
+        year++;
+
+        int a = year / 1000, b = year / 100 % 10, c = year / 10 % 10, d = year % 10;
+
+        if (a != b && a != c && a != d && b != c && b != d && c != d)
+            break;
+    }
+
+    cout << year << endl;
+
+    return 0;
+}
